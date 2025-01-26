2a01a913a87b6089c45f899bb80a0eaf5a292cd7
twkirumba <twkirumba@gmail.com>
2025-01-26 17:10:10 -0500

    Dynamic font color

--- force-app/main/default/lwc/blanketApp_Preview/blanketApp_Preview.js
+++ force-app/main/default/lwc/blanketApp_Preview/blanketApp_Preview.js
@@ -32,10 +32,23 @@ export default class BlanketApp_Preview extends LightningElement {
         data.forEach((item, index) => {
             this.listOfBlanketRows.push({
                 data: item,
-                columnStyle: 'text-align: left; border: 0.5px solid white; border-collapse: collapse; font-size: 13px; line-height: 1; height: 350px; max-width: 1%; padding: 2px; background-color: ' + item.Color_Scheme_Item__r.Color__c + ';',
+                columnStyle: 'text-align: left; border: 0.5px solid white; border-collapse: collapse; font-size: 13px; line-height: 1; height: 350px; max-width: 1%; padding: 2px; background-color: ' + item.Color_Scheme_Item__r.Color__c + '; color: ' + this.getTextColor(item.Color_Scheme_Item__r.Color__c) + ';',
                 index: index
             });
             });
         console.log('example columnStyle --> ' + this.listOfBlanketRows[1].columnStyle);
+    }
+
+    getTextColor(backgroundColor) {
+        // Convert hex to RGB
+        const r = parseInt(backgroundColor.substr(1,2), 16);
+        const g = parseInt(backgroundColor.substr(3,2), 16);
+        const b = parseInt(backgroundColor.substr(5,2), 16);
+        
+        // Calculate luminance
+        const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+        
+        // Return black for light backgrounds, white for dark
+        return luminance > 0.5 ? '#000000' : '#FFFFFF';
     }
 }
