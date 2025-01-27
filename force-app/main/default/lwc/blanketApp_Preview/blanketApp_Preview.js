9d47b1db14c432290ec79acfe3588fb401daa0bc
twkirumba <twkirumba@gmail.com>
2025-01-26 20:36:38 -0500

    Visual Indicator of Days to Be Knit

--- force-app/main/default/lwc/blanketApp_Preview/blanketApp_Preview.js
+++ force-app/main/default/lwc/blanketApp_Preview/blanketApp_Preview.js
@@ -5,38 +5,73 @@ import createBlanketForSchemeAndDateWeather from '@salesforce/apex/BlanketAppSer
 export default class BlanketApp_Preview extends LightningElement {
     @api listOfAccounts;
     @api year;
+    _lastKnitDateInput;
+    @api 
+    get lastKnitDateInput() {
+        return this._lastKnitDateInput;
+    }
+    set lastKnitDateInput(value){
+        this._lastKnitDateInput = value;
+        try{
+            this.createBlanketRows();
+        }catch(e){
+            console.log(e);
+        }
+    }
+    blanketRowData;
     listOfBlanketRows;
     listOfBlanketRowsError;
 
     connectedCallback(){
         this.wire_PreviewBlanket();
+        console.log('knitDateInput: ' + this._lastKnitDateInput);
+        if(!this._lastKnitDateInput){
+            this._lastKnitDateInput = new Date();
+        }
     }
 
     wire_PreviewBlanket(){
         createBlanketForSchemeAndDateWeather({ colorSchemeItemList: this.listOfAccounts, year: this.year })
             .then(data => {
-                console.log(JSON.stringify(data));
-                this.createBlanketRows(data);
+                //console.log(JSON.stringify(data));
+                this.blanketRowData = data;
+                this.createBlanketRows();
             })
             .catch(error => {
-                //console.log(JSON.stringify(error));
+                console.log(JSON.stringify(error));
                 this.listOfBlanketRowsError = JSON.stringify(error);
             }); 
     }
 
-    createBlanketRows(data){
-        console.log('setColumnStyle start');
-        //console.log(JSON.stringify(data[0]));
-        //console.log(JSON.stringify(data[0].Color_Scheme_Item__r.Color__c));
+    createBlanketRows(){
+        console.log('createBlanketRows start');
         this.listOfBlanketRows = [];
-        data.forEach((item, index) => {
+        const lastKnitDate = new Date(this._lastKnitDateInput);
+        console.log('lastKnitDate: ' + lastKnitDate);
+        this.blanketRowData.forEach((item, index) => {
+            const rowDate = new Date(item.DateWeather__r.Date__c);
+            console.log('rowDate: ' + rowDate);
+            console.log(rowDate <= lastKnitDate);
+            const backgroundColor = item.Color_Scheme_Item__r.Color__c;
+            const textColor = this.getTextColor(backgroundColor);
+            
+            // Style based on date comparison
+            const cellStyle = rowDate <= lastKnitDate ? 
+                // Style for past/current dates - full color as before
+                `text-align: left; border: 0.5px solid white; border-collapse: collapse; 
+                 font-size: 13px; line-height: 1; height: 350px; max-width: 1%; 
+                 padding: 2px; background-color: ${backgroundColor}; color: ${textColor};` :
+                // Style for future dates - white background with colored border
+                `text-align: left; border: 10px solid ${backgroundColor}; border-collapse: collapse; 
+                 font-size: 13px; line-height: 1; height: 350px; max-width: 1%; 
+                 padding: 2px; background-color: white; color: black;`;
+    
             this.listOfBlanketRows.push({
                 data: item,
-                columnStyle: 'text-align: left; border: 0.5px solid white; border-collapse: collapse; font-size: 13px; line-height: 1; height: 350px; max-width: 1%; padding: 2px; background-color: ' + item.Color_Scheme_Item__r.Color__c + '; color: ' + this.getTextColor(item.Color_Scheme_Item__r.Color__c) + ';',
+                columnStyle: cellStyle,
                 index: index
             });
-            });
-        console.log('example columnStyle --> ' + this.listOfBlanketRows[1].columnStyle);
+        });
     }
 
     getTextColor(backgroundColor) {
